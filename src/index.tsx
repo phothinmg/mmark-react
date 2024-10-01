c70b16d61b30da186a1a5386acb57f8f2fd2666f
Pho Thin <phothinmg@disroot.org>
2024-10-01 11:13:46 +0630

    modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,17 @@
-"use client";
-import mmmark, { customClass, icons } from "mm-mark";
+import mmmark from "mm-mark";
 import type { MmmarkUserSelectOptions } from "mm-mark";
 import React from "react";
 
-type Opts = Omit<MmmarkUserSelectOptions, "extensions">;
-type Extensions = MmmarkUserSelectOptions["extensions"];
 export type MOptions = {
   content: string;
-  options?: Opts;
-  extensions?: Extensions;
+  options?: MmmarkUserSelectOptions;
 };
 
-const Markdown: React.FC<MOptions> = ({ content, options, extensions }) => {
-  const converter = React.useMemo(
-    () =>
-      mmmark.Converter({
-        ...options,
-        extensions: [icons, customClass, ...extensions],
-      }),
-    [options, extensions]
-  );
-  const cleanMark = React.useMemo(
-    () => mmmark.frontmatter(content).content,
-    [content]
-  );
-  const htmlContent = React.useMemo(
-    () => converter.makeHtml(cleanMark),
-    [converter, cleanMark]
-  );
-  const html = React.useMemo(() => ({ __html: htmlContent }), [htmlContent]);
+const Markdown: React.FC<MOptions> = ({ content, options }) => {
+  const converter = mmmark.Converter(options);
+  const cleanMark = mmmark.frontmatter(content).content;
+  const htmlContent = converter.makeHtml(cleanMark);
+  const html = { __html: htmlContent };
   return <div id="mm-mark" dangerouslySetInnerHTML={html} />;
 };
 
